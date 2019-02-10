c8c3f1ff30ca5b348f931860e8b62c946602c11a
yuanjue <yuanjue@kth.se>
2019-02-10 03:05:44 +0100

    Add Comments in parser.h
    Add comments in source/parser.h to explain the functions

--- source/parser.h
+++ source/parser.h
@@ -16,20 +16,28 @@ class Parser {
 public:
     Parser();
 
+    //Initialize the parser with a given string
     Parser(string json_message);
 
+    //Return the pull request action(opened, edited, closed, merged, etc.)
     string GetAction();
 
+    //Return URL of the pull request
     string GetPrURL();
 
+    //Return clone URL of the repo
     string GrtCloneURL();
 
+    //Return the title of the pull request
     string GetPrTitle();
 
+    //Return the body of the pull request
     string GetPrBody();
 
+    //Return the time and data that the pull request was created
     string GetPrTime();
 
+    //Return the username of the creator of the pull request
     string GetPrUser();
 };
 
