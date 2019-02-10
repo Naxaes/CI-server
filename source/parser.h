ff7a5bc4fc61fd93f11772f444b7557663b97bb9
yuanjue <yuanjue@kth.se>
2019-02-10 02:37:05 +0100

    Delete the comments

--- source/parser.h
+++ source/parser.h
@@ -16,28 +16,20 @@ class Parser {
 public:
     Parser();
 
-    //Initialize a Parser with a given string
     Parser(string json_message);
 
-    //Return the pull request action(including opened, edited, closed, merged, etc.)
     string GetAction();
 
-    //Return the URL of the pull request
     string GetPrURL();
 
-    //Return the clone URL of the repo
     string GrtCloneURL();
 
-    //Return title of the pull request
     string GetPrTitle();
 
-    //Return the body of the pull request
     string GetPrBody();
 
-    //Return the time and data that the pull request
     string GetPrTime();
 
-    //Return the username of the creator of the pull request
     string GetPrUser();
 };
 
